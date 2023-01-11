765235ee76b7a0d29f0f37036afba79443e196f2
zein3 <zeinhaddad02@gmail.com>
2023-01-11 10:22:33 +0700

    deemphasized section header

--- components/section/header.js
+++ components/section/header.js
@@ -2,7 +2,7 @@ export default function Header({ children, dataAos }) {
   return (
     <>
       <div data-aos={dataAos ?? "fade-in"} className="my-4 py-2 px-2 lg:px-0 flex flex-row items-center">
-        <h1 className="font-bold text-white text-2xl mr-2">{ children }</h1>
+        <h1 className="font-semibold text-gray text-xl mr-2">{ children }</h1>
         <span className="border-t-[1px] border-t-darkgray grow"></span>
       </div>
     </>
